c312e4a835c2dc44438a6283e22cf9b4525e6d1d
Sebastian <sm@vivid-planet.com>
2022-08-16 10:34:22 +0200

    remove default AuthorizationGate implementation, remove AuthorizationProvider

--- packages/react-app-auth/src/authorizationcontext/AuthorizationProvider.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-import { AuthorizationContext, AuthorizationContextValues } from "./AuthorizationContext";
-
-type AuthorizationProviderProps = AuthorizationContextValues;
-export const AuthorizationProvider: React.FunctionComponent<AuthorizationProviderProps> = ({ children, ...value }) => {
-    return <AuthorizationContext.Provider value={value}>{children}</AuthorizationContext.Provider>;
-};
